678bf76dcc400320f138c5aa67c5aed826d7399d
Josh Beam <joshua.a.beam@gmail.com>
2015-09-12 13:57:35 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Monocular
+
+Get a live peek into San Francisco landmarks. Know before you go.
+
+Features:
+
+- See current and forecasted weather
+- See the best time to go
+- See an Instagram feed of the latest pictures of the landmark
+- See links to live webcams where available
+
+---
+
+&copy; 2015 Josh Beam - MIT License talk@joshbe.am
---
+++ app/components/Home.jsx
@@ -0,0 +1,25 @@
+import { Reapp, React, NestedViewList, View, Button } from 'reapp-kit';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <NestedViewList {...this.props.viewListProps}>
+        <View title="Monocular">
+          <p>Get a live peek into the major landmarks of San Francisco.</p>
+        </View>
+
+        {this.props.child()}
+      </NestedViewList>
+    );
+  }
+}
+
+export default Reapp(Home);
+
+/*
+ This is your root route. When you wrap it with Reapp()
+ it passes your class two properties:
+
+  - viewListProps: Passes the scrollToStep to your ViewList so it animates
+  - child: The child route
+*/
