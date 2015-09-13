9422c8eadb2d07a69a0359f97081ad1b5d662a1e
Josh Beam <joshua.a.beam@gmail.com>
2015-09-13 15:13:50 -0700

    adds jquery and api class

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Card, Header, Slider } from '../ratchet';
+import Api from '../../data';
 
 export default class extends React.Component {
+  componentWillMount() {
+    new Api().get('landmarks')
+    .then((res) => {
+      console.log(res);
+    });
+  }
+
   render() {
     let sliderChildren = [
       <img src="http://globe-views.com/dcim/dreams/smile/smile-06.jpg" id="1" />,
